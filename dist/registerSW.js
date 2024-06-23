8ece49e8eb92bda5d654e6e0b8da9ec76c744651
nd <nirajdasari@yahoo.com>
2024-06-23 13:41:01 +0530

    fix manifest path

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://github.com/KreativeND/e-commerce/sw.js', { scope: 'https://github.com/KreativeND/e-commerce/' })})}
