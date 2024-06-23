0c350dc1d12f393643470db88f000059c83349a9
KreativeND <155289206+KreativeND@users.noreply.github.com>
2024-06-23 08:37:25 +0000

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/KreativeND/e-commerce",
+  base: "/e-commerce/",
   plugins: [
     react(),
     VitePWA({
